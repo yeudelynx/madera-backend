d92046567188c0f5931849a994a649505fa62a2f
Matthieu <pauly.matthieu@gmail.com>
2018-04-17 20:00:10 +0200

    CLOSED : fix sync bug when request input miss

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -16,7 +16,12 @@ class SyncController extends Controller
 		/*
 			TODO : Replace postJson with json from request
 		*/
+	    var_dump($request->input('date'));
+    	var_dump($request->input('clients'));
+    	var_dump($request->input('devis'));
+    	var_dump($request->input('constituers'));
     	$syncRequest = new SyncRequest($request->input('date'), $request->input('clients'), $request->input('devis'), $request->input('constituers'));
+
     	$syncRequest->Process();
 
     	$syncResponse = new SyncResponse();
--- app/SyncRequest.php
+++ app/SyncRequest.php
@@ -26,15 +26,12 @@ class SyncRequest
 
         //Save Clients & update Devis->client_id with fresh mysql id
         if(!empty($this->arrayClients)){            
-            foreach ($this->arrayClients as $key => $cli){
+            foreach ($this->arrayClients as $key => &$cli){
                 //Check if client exist (created_at != NULL)
                 if($cli['created_at'] == NULL){
                     //New resource so store that in server
                     $oldId = $cli['id'];
                     unset($cli['id']);
-                    unset($cli['created_at']);
-                    unset($cli['updated_at']);
-                    unset($cli['deleted_at']);
                     $client = new Client();
                     $client->fill($cli);
                     $client->save();
@@ -54,15 +51,12 @@ class SyncRequest
         }
         //Save Devis & update Constituers->devis_id with fresh mysql id
         if(!empty($this->arrayDevis)){
-            foreach ($this->arrayDevis as $key => $dev){
+            foreach ($this->arrayDevis as $key => &$dev){
                 //Check if devis exist (created_at != NULL)
                 if($dev['created_at'] == NULL){
                     //New resource so store that in server
                     $oldId = $dev['id'];
                     unset($dev['id']);
-                    unset($dev['created_at']);
-                    unset($dev['updated_at']);
-                    unset($dev['deleted_at']);
                     $devis = new Devis();
                     $devis->fill($dev);
                     $devis->save();
@@ -82,16 +76,11 @@ class SyncRequest
         }
 
         if(!empty($this->arrayConstituers)){
-            var_dump('ici 1 1 1');
-            foreach ($this->arrayConstituers as $key => $const){
+            foreach ($this->arrayConstituers as $key => &$const){
                 //Check if client exist (created_at != NULL)
-                var_dump('ici 2 2 2');
                 if($const['created_at'] == NULL){
                     //New resource so store that in server
                     $constituer = new Constituer();
-                    unset($const['created_at']);
-                    unset($const['updated_at']);
-                    unset($const['deleted_at']);
                     $constituer->fill($const);
                     $constituer->save();
                 }else{
